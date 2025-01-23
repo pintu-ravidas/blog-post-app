7364493e2f93afc6496e808d2f429bb0f5f66915
Pintu Ravidas <74536453+pintu-ravidas@users.noreply.github.com>
2025-01-23 20:35:47 +0530

    Update signup.js

--- src/routes/auth/signup.js
+++ src/routes/auth/signup.js
@@ -5,7 +5,7 @@ import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
-router.post('/api/user/signup',
+router.post(process.env.BASE_URL+'/api/user/signup',
      [
         body('email').notEmpty().isEmail().withMessage('Invalid email id'),
         body('password').notEmpty().isLength({ min: 5}).withMessage('Password must be 5 character long')
